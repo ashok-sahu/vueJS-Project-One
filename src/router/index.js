72d7b7c3bbdeec2876439ed362c2af98419007dd
ashok-sahu <ashoksahu1105@gmail.com>
2020-09-16 01:18:21 +0530

    completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
-import Projects from '../views/Projets.vue'
-import Team from '../views/Team.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Projects from "../views/Projects.vue";
+import Team from "../views/Team.vue";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'dashboard',
-    component: Dashboard
+    path: "/",
+    name: "dashboard",
+    component: Dashboard,
   },
   {
-    path: '/team',
-    name: '',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/projects",
+    name: "projects",
+    component: Projects,
+  },
+  {
+    path: "/team",
+    name: "team",
+    component: Team,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
